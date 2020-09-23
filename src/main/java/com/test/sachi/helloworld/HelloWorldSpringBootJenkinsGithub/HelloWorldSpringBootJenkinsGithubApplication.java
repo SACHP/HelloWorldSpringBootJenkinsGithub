0a4eb1b0837ff0c2084927e4bc6ad14cdf7186eb
Sachidananda <sachida23@gmail.com>
2020-09-24 01:05:57 +0530

    This is my first git jenkinscommit

---
+++ src/main/java/com/test/sachi/helloworld/HelloWorldSpringBootJenkinsGithub/HelloWorldSpringBootJenkinsGithubApplication.java
@@ -0,0 +1,13 @@
+package com.test.sachi.helloworld.HelloWorldSpringBootJenkinsGithub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldSpringBootJenkinsGithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldSpringBootJenkinsGithubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/sachi/helloworld/HelloWorldSpringBootJenkinsGithub/HelloWorldSpringBootJenkinsGithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.sachi.helloworld.HelloWorldSpringBootJenkinsGithub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldSpringBootJenkinsGithubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
